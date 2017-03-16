02fa43c4ba7531184f56c6d2451b5b89b90e098e
Francois 'ftiff' Levaux-Tiffreau <fti@me.com>
2017-03-16 21:06:28 +0100

    Initial Commit

---
+++ TMP/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TMP
+//
+//  Created by Francois Levaux on 16.03.17.
+//  Copyright © 2017 Francois Levaux. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
